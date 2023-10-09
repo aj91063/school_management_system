3ddc79fc518c15649ecba5d5defaba8c63a93b5d
Karan <aj91063@gmail.com>
2023-10-09 19:24:42 +0530

    implementing h2 database

--- src/main/java/com/aj/mvc1/controller/ContactController.java
+++ src/main/java/com/aj/mvc1/controller/ContactController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
